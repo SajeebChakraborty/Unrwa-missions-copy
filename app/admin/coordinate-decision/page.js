f41c0b896f19a0a181699a2e8047cddd30fd319f
Sajeeb Chakraborty <48250220+SajeebChakraborty@users.noreply.github.com>
2024-03-25 09:19:34 +0600

    Update page.js

--- app/admin/coordinate-decision/page.js
+++ app/admin/coordinate-decision/page.js
@@ -80,7 +80,7 @@ function CoordinationDecision() {
                                  <button
                                   onClick={async () => {
                                       // Show a confirmation alert
-                                      const confirmed = window.confirm("Are you sure you want to delete?");
+                                      const confirmed = 1;
 
                                       if (confirmed) {
                                           // Make a DELETE request to your API to mark the question as deleted
