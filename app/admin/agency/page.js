55a1207067d95580bbe34c666b2f687002cec024
Sajeeb Chakraborty <48250220+SajeebChakraborty@users.noreply.github.com>
2024-03-25 09:17:44 +0600

    Update page.js

--- app/admin/agency/page.js
+++ app/admin/agency/page.js
@@ -145,7 +145,7 @@ function AgencyList() {
                                  <button
                                   onClick={async () => {
                                       // Show a confirmation alert
-                                      const confirmed = window.confirm("Are you sure you want to delete?");
+                                      const confirmed = 1;
 
                                       if (confirmed) {
                                           // Make a DELETE request to your API to mark the question as deleted
